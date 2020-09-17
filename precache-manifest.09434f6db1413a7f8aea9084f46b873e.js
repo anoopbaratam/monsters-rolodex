0f90788d7e74a742a7ccc2e6e242018072b449b4
Anoop Baratam <anoopbaratam@gmail.com>
2020-09-17 14:41:04 +0530

    Updates

---
+++ precache-manifest.09434f6db1413a7f8aea9084f46b873e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08fd4efe359d0c0eca294f4d8bda70f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8546da21633bb6d55da0",
+    "url": "/monsters-rolodex/static/css/main.ea90c6d6.chunk.css"
+  },
+  {
+    "revision": "398406a8997887bb3004",
+    "url": "/monsters-rolodex/static/js/2.da4dd827.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.da4dd827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8546da21633bb6d55da0",
+    "url": "/monsters-rolodex/static/js/main.27ec9c3a.chunk.js"
+  },
+  {
+    "revision": "5690351d0588d7ab7df7",
+    "url": "/monsters-rolodex/static/js/runtime-main.e694af05.js"
+  }
+]);
